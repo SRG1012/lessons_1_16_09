f0367140f33ee2af050315d324e56fb7b80c1f30
SRG1012 <didenko1012@gmail.com>
2024-09-21 19:55:24 +0300

    lesson_1: вводимо число, вираховуємо кожну цифру, методом цілочисленного ділення на 10. lwsson_2:вводимо число,розбиваємо число на цифри методом цілочисленного ділення на 10, та виводимо на єкран

--- lesson_1.py
+++ lesson_1.py
@@ -1,1 +1,13 @@
-print("Hello world")
+
+print("Введіть чотиризначне число:")
+x = int (input ())
+x1 =x//1000%10
+x2 =x//100%10
+x3 =x//10%10
+x4 =x%10
+print(x1)
+print(x2)
+print(x3)
+print(x4)
+
+
