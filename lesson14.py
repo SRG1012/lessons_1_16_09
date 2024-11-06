d74c8711e751b46094a714ccaad64916da4108f6
SRG1012 <didenko1012@gmail.com>
2024-11-06 10:56:01 +0200

    1.Визначити популярність певних слів у тексті 2.Різниця між числами

---
+++ lesson14.py
@@ -0,0 +1,82 @@
+class GroupLimitError(Exception):
+
+    def __init__(self, message="Кількість студентів у групі не може перевищувати 10"):
+        self.message = message
+        super().__init__(self.message)
+
+
+class Human:
+    def __init__(self, gender, age, first_name, last_name):
+        self.gender = gender
+        self.age = age
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}, Gender: {self.gender}, Age: {self.age}'
+
+
+class Student(Human):
+    def __init__(self, gender, age, first_name, last_name, record_book):
+        super().__init__(gender, age, first_name, last_name)
+        self.record_book = record_book
+
+    def __str__(self):
+        return f'{super().__str__()}, Record Book: {self.record_book}'
+
+    def __eq__(self, other):
+        return isinstance(other, Student) and self.record_book == other.record_book
+
+    def __hash__(self):
+        return hash(self.record_book)
+
+
+class Group:
+    def __init__(self, number):
+        self.number = number
+        self.group = set()
+
+    def add_student(self, student):
+        if len(self.group) >= 10:
+            raise GroupLimitError()
+        self.group.add(student)
+
+    def delete_student(self, last_name):
+        student_to_delete = self.find_student(last_name)
+        if student_to_delete:
+            self.group.remove(student_to_delete)
+
+    def find_student(self, last_name):
+        for student in self.group:
+            if student.last_name == last_name:
+                return student
+        return None
+
+    def __str__(self):
+        all_students = '\n'.join(str(student) for student in self.group)
+        return f'Number: {self.number}\n{all_students}'
+
+
+# Тестовий код
+st1 = Student('Male', 30, 'Steve', 'Jobs', 'AN142')
+st2 = Student('Female', 25, 'Liza', 'Taylor', 'AN145')
+gr = Group('PD1')
+gr.add_student(st1)
+gr.add_student(st2)
+print(gr)
+assert str(gr.find_student('Jobs')) == str(st1), 'Test1'
+assert gr.find_student('Jobs2') is None, 'Test2'
+assert isinstance(gr.find_student('Jobs'), Student) is True, 'Метод поиска должен возвращать экземпляр'
+
+gr.delete_student('Taylor')
+print(gr)  # Only one student
+
+gr.delete_student('Taylor')  # No error!
+
+for i in range(10):
+    gr.add_student(Student('Male', 20 + i, f'Name{i}', f'LastName{i}', f'AN14{i}'))
+print (gr)
+try:
+    gr.add_student(st1)
+except GroupLimitError as a:
+    print(a)
